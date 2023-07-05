8d6da627740b1a3694c32f93f7c4ccf97735c441
Дмитрий <dimulatoraz@gmail.com>
2023-07-05 12:55:27 +0700

    Начал

--- Transfusion/main.c
+++ Transfusion/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <locale.h>
+
 
 int main()
 {
-	
+	setlocale(LC_ALL, "Rus");
+	int first = 0, second = 0, third = 0;
+	printf("Введите объём первого сосуда: ");
+	scanf("%d", first);
+	printf("Введите объём второго сосуда: ");
+	scanf("%d", second);
+	printf("Введите объём третьего сосуда: ");
+	scanf("%d", third);
+
+
 }
